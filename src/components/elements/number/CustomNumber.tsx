8c30cc796d66b3727b36a471ee77bdadf77048be
jhbeltran01 <jhbeltran2001@gmail.com>
2022-02-22 15:16:42 +0800

    create Custom Number component

---
+++ src/components/elements/number/CustomNumber.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CustomNumber = (): JSX.Element => {
+  return (
+    <div>
+      <button>-</button>
+      <input type="number" />
+      <button>+</button>
+    </div>
+  )
+}
+
+export default CustomNumber
