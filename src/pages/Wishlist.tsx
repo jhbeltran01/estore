bb362698239a75fdb85ad1988f3e0588dd800489
jhbeltran01 <jhbeltran2001@gmail.com>
2022-03-04 09:46:24 +0800

    add Header component in Wishlist page

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -4,6 +4,7 @@ import '@Utils/toggleMobileNavMenu'
 import TableHeader from '@Components/elements/table/TableHeader';
 import WishlistTableRow from '@Page-section/Wishlist/WishlistTableRow';
 import products from '@Json/products.json';
+import Header from '@Components/header/Header';
 
 const headers = ['Product', 'Price', 'Quantity', 'Add To Cart', 'Remove'];
 
@@ -16,27 +17,36 @@ const Wishlist = (): JSX.Element => {
   }
 
   return (
-    <div className='pad-3 pad-2 mar-top-res'>
-      <div className='overflow-scroll pad-10 bg-white-2'>
-        <table className='table-3'>
-          <TableHeader headers={headers} className='table-3__header' />
-          <tbody>
-            {
-              wishlist.map((product, index) =>
-                <WishlistTableRow
-                  key={product.id}
-                  id={product.id}
-                  imgSrc={product.imgSrc}
-                  name={product.name}
-                  price={product.prize}
-                  updateWishlist={updateWishlist}
-                />
-              )
-            }
-          </tbody>
-        </table>
+    <React.Fragment>
+      {
+        ReactDOM.createPortal(
+          <Header />,
+          document.getElementById('search-bar')!
+        )
+      }
+
+      <div className='pad-3 pad-2 mar-top-res'>
+        <div className='overflow-scroll pad-10 bg-white-2'>
+          <table className='table-3'>
+            <TableHeader headers={headers} className='table-3__header' />
+            <tbody>
+              {
+                wishlist.map((product, index) =>
+                  <WishlistTableRow
+                    key={product.id}
+                    id={product.id}
+                    imgSrc={product.imgSrc}
+                    name={product.name}
+                    price={product.prize}
+                    updateWishlist={updateWishlist}
+                  />
+                )
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   )
 }
 
