b36a71fffb1455906e090b7bf802ce1db5c0360d
jhbeltran01 <jhbeltran2001@gmail.com>
2022-03-14 12:15:55 +0800

    add disabled star in rating

--- src/hooks/useRating.tsx
+++ src/hooks/useRating.tsx
@@ -4,7 +4,12 @@ const useRating = (rating: number): JSX.Element[] => {
   const stars: JSX.Element[] = []
 
   for (let i = 0; i < rating; ++i) {
-    stars.push(<i key={i} className="star fa fa-star" > </i>)
+    stars.push(<i key={i} className="active-star fa fa-star" > </i>)
+  }
+  console.log(rating)
+
+  for (let i = rating; i < 5; ++i) {
+    stars.push(<i key={i} className="disabled-star fa fa-star" > </i>)
   }
 
   return stars;
