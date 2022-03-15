df54c45c4b3e8e7d96181af946d9e01e05baa826
jhbeltran01 <jhbeltran2001@gmail.com>
2022-03-15 08:48:24 +0800

    add arrow functionality on Sliders

--- src/hooks/transitions/useSliderTransition.tsx
+++ src/hooks/transitions/useSliderTransition.tsx
@@ -6,33 +6,81 @@ type UseSliderTransitionProps = {
 }
 
 const useSliderTransition = ({ name, contentLength, intervalTime, numberOfClones }: UseSliderTransitionProps) => {
-  const carousel = document.getElementById(`js-${name}-carousel`) as HTMLDivElement;
-  const slider = carousel.querySelector(`#js-${name}-slider`) as HTMLDivElement;
-  const content = carousel.querySelector('#js-content') as HTMLDivElement;
+  const carouselContainer = document.getElementById(`js-${name}-container`) as HTMLDivElement;
+  const slider = carouselContainer.querySelector(`#js-${name}-slider`) as HTMLDivElement;
+  const content = carouselContainer.querySelector('#js-content') as HTMLDivElement;
+  const leftArrow = carouselContainer.querySelector('#js-arrow-left') as HTMLDivElement;
+  const rightArrow = carouselContainer.querySelector('#js-arrow-right') as HTMLDivElement;
 
   let displayedContent = numberOfClones - 1;
+  let sliderInterval: NodeJS.Timer;
 
-  slider.style.transform = `translateX(-${content.clientWidth * displayedContent}px)`;
-
-
-  const sliderInterval = setInterval(() => {
-    ++displayedContent;
+  const slide = () => {
     slider.style.transition = '500ms ease-in-out'
     slider.style.transform = `translateX(-${content.clientWidth * displayedContent}px)`;
-  }, intervalTime)
+  }
+
+
+
+  const slideLeft = (): void => {
+    if (displayedContent > contentLength - numberOfClones) { return }
+
+    ++displayedContent;
+    slide();
+  }
+
+
+
+  const slideRight = (): void => {
+    if (displayedContent <= 0) { return }
+    --displayedContent;
+    slide();
+  }
+
+
+
+  const changeSlideDirection = (direction: () => void) => {
+    clearInterval(sliderInterval);
+
+    direction()
+
+    sliderInterval = setInterval(direction, intervalTime)
+  }
+
+
+
+  const rightArrowClickHandler = () => changeSlideDirection(slideLeft)
+  const leftArrowClickHandler = () => changeSlideDirection(slideRight)
+
+  leftArrow.addEventListener('click', leftArrowClickHandler);
+  rightArrow.addEventListener('click', rightArrowClickHandler);
+
 
 
   const transitionEndHandler = (): void => {
-    if (displayedContent === contentLength - numberOfClones) {
+    const hasReachedLastElement = displayedContent === contentLength - numberOfClones;
+    const hasReachedFirstElement = displayedContent === 0;
+
+    if (hasReachedLastElement) {
       slider.style.transition = 'none'
-      slider.style.transform = `translateX(-${0}px)`;
+      slider.style.transform = `translateX(0px)`;
       displayedContent = 0;
+    } else if (hasReachedFirstElement) {
+      slider.style.transition = 'none';
+      slider.style.transform = `translateX(-${content.clientWidth * (contentLength - numberOfClones)}px)`;
+      displayedContent = contentLength - numberOfClones
+
+    } else {
+      // do nothing
     }
   }
-
   slider.addEventListener('transitionend', transitionEndHandler)
 
-  return { slider, sliderInterval, transitionEndHandler }
+
+  slider.style.transform = `translateX(-${content.clientWidth * displayedContent}px)`;
+  sliderInterval = setInterval(slideLeft, intervalTime)
+
+  return { slider, sliderInterval, transitionEndHandler, leftArrow, leftArrowClickHandler, rightArrow, rightArrowClickHandler }
 }
 
 export default useSliderTransition
